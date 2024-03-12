685206e7ceab61b71089b7782ce1b226baf7efa3
Korndanai <korndanai.likit@gmail.com>
2024-03-12 13:16:14 +0700

    Initial Commit

---
+++ PlantGuide/PlantGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantGuideApp.swift
+//  PlantGuide
+//
+//  Created by Korndanai Likitpattanakul on 12/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlantGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
